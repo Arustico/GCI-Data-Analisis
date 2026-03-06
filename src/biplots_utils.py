a1b78b17f4879ff2f9a7fc2252e5a91f8a7c6d18
Ariel <ariel.nunez.sa@gmail.com>
2026-03-06 18:38:29 -0300

    Instalación de librerías adjustText y sckit-learn

--- src/biplots_utils.py
+++ src/biplots_utils.py
@@ -11,11 +11,18 @@ Modulo con funciones útiles para el análisis con biplot
 import unicodedata
 import re
 from pathlib import Path
+import os
 from dotenv import load_dotenv
+import logging
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+import seaborn as sns
+
 import plotly.express as px
 import plotly.graph_objects as go
+from pybiplots import *
 
 # ─────────────────────────────────────────────
 # Configuración de entorno y logging
@@ -41,7 +48,6 @@ TABLE_FOLDER = Path(os.getenv("TABLE_FOLDER"))
 # Funciones
 #─────────────────────────────────
 
-
 def normalizar_puntajes(values: pd.Series) -> pd.Series:
     """
     Normaliza puntajes de paises acorde a la fórmula de la metodología 4.0 del WEF
@@ -51,143 +57,142 @@ def normalizar_puntajes(values: pd.Series) -> pd.Series:
     frontera = values.max()
     return (values-wpi)/(frontera-wpi) * 100
 
-def normalizar_text(texto):
 
-    texto = str(texto)
-    # Quitar acentos
-    texto = ''.join(
-                c for c in unicodedata.normalize('NFD', texto)
-                    if unicodedata.category(c) != 'Mn'
-                )
-    # Todo minúsculas
-    texto = texto.lower()
-    # eliminar puntuación, comas, asteriscos, etc.
-    texto = re.sub(r'[^a-z0-9\s]', '', texto)
-    # eliminar espacios múltiples
-    texto = re.sub(r'\s+', ' ', texto).strip()
-    return texto
+def savefig(fig: pl.fig,
+            name: str,
+            folder: Path = FIG_FOLDER) -> None:
+    """
+    Guarda las figuras en folder, para ser utilizadas en docs
+    """
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    filename = f"{folder}/{name}.png"
+    fig.savefig(filename,dpi=300,bbox_inches='tight')
+    logging.info(f"Figura guardada en: {filename}")
 
 
-def savefig(fig,name,folder=FIG_FOLDER):
-  if not os.path.exists(folder):
-    os.makedirs(folder)
-  pathtosave = os.path.join(folder,name + ".png")
-  fig.savefig(pathtosave,dpi=300,bbox_inches='tight')
-  print(f"Figura guardada en: {pathtosave}")
-
-def savetableview(df,name,folder=TABLE_FOLDER):
-  if not os.path.exists(folder):
-    os.makedirs(folder)
-  pathtosave = os.path.join(folder,name + ".xlsx")
-  df.to_excel(pathtosave,index=False)
-  print(f"Tabla guardada en: {pathtosave}")
+def savetableview(df: pd.DataFrame,
+                  name: str,
+                  folder: Path = TABLE_FOLDER) -> None:
+    """
+    Guarda tablas para análisis y muestras en formato excel.
+    """
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    filename = f"{folder}/{name}.xlsx"
+    df.to_excel(filename,index=False)
+    logging.info(f"Tabla guardada en: {filename}")
 
 
-
-import plotly.express as px
-import plotly.graph_objects as go
-
-def crear_biplot_interactivo(HJ, groups=None, text_label=None,
+def crear_biplot_interactivo(HJ: HJ_Biplot.fit,
+                             groups=None, text_label=None,
                              axis_x=0, axis_y=1, arrow_color='gray',
-                             arrow_head=2, marker_size=12, title=None):
+                             arrow_head=2, marker_size=12, title=None) -> tuple(px.figure,np.array):
+    """
+    Crea un biplot interactivo. HJ puede ser también GH, JK.
+    """
 
-  # --- Datos base ---
-  X = HJ.X
-  ind = HJ.row_coordinates.reset_index() # corresponden a los individuos
-  vec = HJ.column_coordinates.copy() # corresponden a los vectores
+    # --- Datos base ---
+    X = HJ.X
+    ind = HJ.row_coordinates.reset_index()  # corresponden a los individuos
+    vec = HJ.column_coordinates.copy()      # corresponden a los vectores
 
-  # --- Nombres de ejes ---
-  axis1_name = f'Axis {axis_x + 1} ({HJ.explained_variance[axis_x]:.2f}%)'
-  axis2_name = f'Axis {axis_y + 1} ({HJ.explained_variance[axis_y]:.2f}%)'
-  ind[axis1_name] = ind[f'Axis {axis_x + 1}']
-  ind[axis2_name] = ind[f'Axis {axis_y + 1}']
+    # --- Nombres de ejes ---
+    axis1_name = f'Axis {axis_x + 1} ({HJ.explained_variance[axis_x]:.2f}%)'
+    axis2_name = f'Axis {axis_y + 1} ({HJ.explained_variance[axis_y]:.2f}%)'
+    ind[axis1_name] = ind[f'Axis {axis_x + 1}']
+    ind[axis2_name] = ind[f'Axis {axis_y + 1}']
 
   # --- Gráfico de individuos ---
-  fig = px.scatter(
-    ind, x=axis1_name, y=axis2_name,
-    color=groups, text=text_label,
-    title=title or "Biplot Interactivo"
-  )
-  fig.update_traces(textposition="bottom right", marker_size=marker_size)
+    fig = px.scatter(
+        ind, x=axis1_name, y=axis2_name,
+        color=groups, text=text_label,
+        title=title or "Biplot Interactivo"
+    )
+    fig.update_traces(textposition="bottom right", marker_size=marker_size)
 
-  # --- Añadir autovectores ---
-  for i in range(len(vec)):
-    fig.add_trace(
-    go.Scatter(
-      x=[0, vec.iloc[i, axis_x]],
-      y=[0, vec.iloc[i, axis_y]],
-      mode='lines+text',
-      line=dict(color=arrow_color, width=2),
-      marker=dict(size=20,symbol="arrow-bar-up", angleref="previous",color="black"),
-      text=[None, vec.index[i]],
-      textposition="top center",
-      showlegend=False,
-      name=vec.index[i],
-      hoverinfo='text',
-      hovertext=f"{vec.index[i]}"
-    ))
+    # --- Añadir autovectores ---
+    for i in range(len(vec)):
+        fig.add_trace(
+        go.Scatter(
+                x=[0, vec.iloc[i, axis_x]],
+                y=[0, vec.iloc[i, axis_y]],
+                mode='lines+text',
+                line=dict(color=arrow_color, width=2),
+                marker=dict(size=20,symbol="arrow-bar-up", angleref="previous",color="black"),
+                text=[None, vec.index[i]],
+                textposition="top center",
+                showlegend=False,
+                name=vec.index[i],
+                hoverinfo='text',
+                hovertext=f"{vec.index[i]}"
+            )
+        )
 
-  # --- Layout base ---
-  fig.update_layout(
-    xaxis_title=axis1_name,
-    yaxis_title=axis2_name,
-    template="plotly_white",
-    title_x=0.5
-  )
-  return fig, vec
+    # --- Layout base ---
+    fig.update_layout(
+        xaxis_title=axis1_name,
+        yaxis_title=axis2_name,
+        template="plotly_white",
+        title_x=0.5
+    )
+    return fig, vec
 
 
+def agregar_filtros_biplot(fig: px.Figure,
+                           vec: np.array,
+                           filter_cols: list,
+                           num_previous_filters: int = 3) -> px.Figure:
+    """
+    Agrega filtros a la figura interactiva dash
+    """
+    n_vectores = len(vec)
+    # muestra los elementos (false no los muestra)
+    base_visibility = [True] + [True] * n_vectores  # puntos + vectores
 
-def agregar_filtros_biplot(fig, vec, filter_cols=list,num_previous_filters=3):
+    dropdowns = []
+    x_pos = 0.15
 
-  n_vectores = len(vec)
-  # muestra los elementos (false no los muestra)
-  base_visibility = [True] + [True] * n_vectores  # puntos + vectores
+    for filtro in filter_cols:
+        if filtro not in vec.columns:
+            logging.error(f"Error filtro: {filtro}, no existe columna")
+            return None
+        # valores únicos del filtro
+        valores = sorted(vec[filtro].dropna().unique())
+        buttons = [
+        dict(
+                label=f"{filtro}: Todos",
+                method="update",
+                args=[{"visible": base_visibility}]
+            )
+        ]
+        for val in valores:
+            visible_mask = num_previous_filters * [True] + vec[filtro].eq(val).tolist()
+            buttons.append(
+                dict(
+                label=str(val),
+                method="update",
+                args=[{"visible": visible_mask}]
+            ))
+        #return val,visible_mask
 
-  dropdowns = []
-  x_pos = 0.15
+        dropdowns.append(dict(
+            buttons=buttons,
+            direction="down",
+            showactive=True,
+            x=x_pos,
+            y=1.15,
+            xanchor="left",
+            yanchor="top",
+            pad={"r": 10, "t": 10},
+            active=0,
+            name=filtro.capitalize()
+        ))
+        x_pos += 0.15  # separar menús visualmente
 
-  for filtro in filter_cols:
-    if filtro not in vec.columns:
-      print("Error filtro, no existe columna")
-      return None
-    # valores únicos del filtro
-    valores = sorted(vec[filtro].dropna().unique())
-    buttons = [
-      dict(
-        label=f"{filtro}: Todos",
-        method="update",
-        args=[{"visible": base_visibility}]
-      )
-    ]
-    for val in valores:
-      visible_mask = num_previous_filters * [True] + vec[filtro].eq(val).tolist()
-      buttons.append(
-          dict(
-          label=str(val),
-          method="update",
-          args=[{"visible": visible_mask}]
-      ))
-    #return val,visible_mask
+    fig.update_layout(
+        updatemenus=dropdowns,
+        margin=dict(t=100)
+    )
 
-    dropdowns.append(dict(
-      buttons=buttons,
-      direction="down",
-      showactive=True,
-      x=x_pos,
-      y=1.15,
-      xanchor="left",
-      yanchor="top",
-      pad={"r": 10, "t": 10},
-      active=0,
-      name=filtro.capitalize()
-    ))
-
-    x_pos += 0.15  # separar menús visualmente
-
-  fig.update_layout(
-    updatemenus=dropdowns,
-    margin=dict(t=100)
-  )
-
-  return fig
+    return fig
